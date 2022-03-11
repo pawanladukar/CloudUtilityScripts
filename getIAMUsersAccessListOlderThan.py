958fcf0425e455d7cd5e5505bde8e11dbb9c0af5
pawanladukar <52037731+pawanladukar@users.noreply.github.com>
2022-03-11 00:26:52 -0500

    Update getIAMUsersAccessListOlderThan.py

--- getIAMUsersAccessListOlderThan.py
+++ getIAMUsersAccessListOlderThan.py
@@ -1,5 +1,10 @@
+# AWS IAM Access Keys for all Users with Threshold older than creation date.
+
 import boto3
 from datetime import datetime, timezone
+
+# Threshold In Days
+ThresholdInDays = 30
 
 def utc_to_local(utc_dt):
     return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
@@ -11,6 +16,10 @@ resource = boto3.resource('iam')
 client = boto3.client("iam")
 
 KEY = 'LastUsedDate'
+
+print("================================================================================")
+print("IAM Access Keys List created than " + str(ThresholdInDays) + " Days...")
+print("================================================================================")
 
 for user in resource.users.all():
     Metadata = client.list_access_keys(UserName=user.user_name)
@@ -24,5 +33,7 @@ for user in resource.users.all():
             numOfDays = diff_dates(utc_to_local(datetime.utcnow()), utc_to_local(CreatedDate))
             LastUsed = client.get_access_key_last_used(AccessKeyId=AccessId)
 
-            if ( numOfDays > 30 ):
+            if ( numOfDays > ThresholdInDays ):
                 print("User:", user.user_name,  "Key:", AccessId, "Age of Key:", numOfDays, "Days")
+
+print("================================================================================")
